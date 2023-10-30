88ee588777c99461df7713e282413bb9861c7b17
MAHENDIRAN RAVICHANDRAN <142421172+Mahendiran7@users.noreply.github.com>
2023-10-30 14:43:44 +0530

    new

--- Form.js
+++ Form.js
@@ -1,105 +1,175 @@
-import React, { useState } from 'react'
-import toast from 'react-hot-toast'
-
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import Select from "react-select";
 function Form() {
-    const [student, setstudent]= useState({
-        firstname:"",
-        lastname:"",
-        email:"",
-        password:"",
-    })
-    const [list,setlist] = useState([])
-   const handlechange = (e) =>{
-   // console.log(e.target.name,e.target.value);
-   setstudent({...student,[e.target.name]:e.target.value})
- }
- const handlesubmit = ()=>{
-    if(student.firstname===""){
-        toast.error("firstname required")
-        return
+  const [student, setstudent] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    location:"",
+  });
+
+  const locationoption = [
+    {label:"chennai",value:"chennai"},
+    {label:"covai",value:"covai"},
+    {label:"trichy",value:"trichy"},
+    {label:"madurai",value:"madurai"}
+  ]
+
+  const hobbiesoption =[
+    
+       {label:"cricket",value:"cricket"},
+       {label:"drawing",value:"drawing"},
+       {label:"reading",value:"reading"},
+       {label:"movies",value:"movies"}
+    
+  ]
+
+  const [list, setlist] = useState([]);
+  const handlechange = (e) => {
+    // console.log(e.target.name,e.target.value);
+    setstudent({ ...student, [e.target.name]: e.target.value });
+  };
+  const handlesubmit = () => {
+    if (student.firstname === "") {
+      toast.error("firstname required");
+      return;
     }
-    if(student.lastname===""){
-        toast.error("lastname required")
-        return
+    if (student.lastname === "") {
+      toast.error("lastname required");
+      return;
     }
     {
-        if(student.email===""){
-            toast.error("email required")
-            return
+      if (student.email ==="") {
+        toast.error("email required");
+        return;
+      }
+      {
+        if (student.password ==="") {
+          toast.error("password required");
+          return;
         }
-        {
-            if(student.password===""){
-                toast.error("password required")
-                return
-            }
-        }
+      }
     }
-    toast.success("form submited")
+    toast.success("form submited");
     setstudent({
-        firstname:"",
-        lastname:"",
-        email:"",
-        password:"",
-    })
-    setlist([...list,student])
-    
- }
- console.log(list);
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: "",
+    });
+    setlist([...list, student]);
+  };
+  console.log(student);
   return (
     <>
-    <div className="container mt-5 w-50">
+      <div className="container mt-5 w-50">
         <h2>Create student</h2>
-        <div className='row'>
-            <div className='col-6'>
-                <lable class="form-lable">first name</lable>
-                <input type="text" class="form-control" name="firstname" value={student.firstname} onChange={(e)=>handlechange(e)}/>
+        <div className="row">
+          <div className="col-6">
+            <lable class="form-lable">first name</lable>
+            <input
+              type="text"
+              class="form-control"
+              name="firstname"
+              value={student.firstname}
+              onChange={(e) => handlechange(e)}
+            />
+          </div>
+          <div className="col-6">
+            <lable class="form-lable">Last name</lable>
+            <input
+              type="text"
+              class="form-control"
+              name="lastname"
+              value={student.lastname}
+              onChange={(e) => handlechange(e)}
+            ></input>
+          </div>
+          <div className="col-6">
+            <lable class="form-lable">Email</lable>
+            <input
+              type="email"
+              class="form-control"
+              name="email"
+              value={student.email}
+              onChange={(e) => handlechange(e)}
+            ></input>
+          </div>
+          <div className="col-6">
+            <lable class="form-lable">Password</lable>
+            <input
+              type="password"
+              class="form-control"
+              name="password"
+              value={student.password}
+              onChange={(e) => handlechange(e)}
+            ></input>
+          </div>
+          <div className="col-6">
+            <lable class="form-lable">location</lable>
+            <Select options={locationoption} onChange={(e)=>setstudent({...student,location:e.value})}/>
+          </div>
+          <div className="col-6">
+            <lable class="form-lable">Hobbies</lable>
+            <Select isMulti options={hobbiesoption}  onChange={(e)=>setstudent({...student,hobbies:e.value})}/>
+          </div>
+        </div>
+        <div className="col-6">
+            <div>
+                <input type="radio" onChange={(e)=>console.log(e)}/>
+                <label>Male</label>
             </div>
-            <div className='col-6'>
-                <lable class="form-lable">Last name</lable>
-                <input type='text'class="form-control" name="lastname" value={student.lastname} onChange={(e)=>handlechange(e)} ></input>
-            </div>
-            <div className='col-6'>
-                <lable class="form-lable">Email</lable>
-                <input type='email' class="form-control" name="email" value={student.email} onChange={(e)=>handlechange(e)}  ></input>
-            </div>
-            <div className='col-6'>
-                <lable class="form-lable">Password</lable>
-                <input type='password' class="form-control" name="password" value={student.password} onChange={(e)=>handlechange(e)} ></input>
+            <div>
+                <input type="radio"></input>
+                <label>Female</label>
             </div>
         </div>
-        <div className='text-end mt-3'>
-            <button className='btn btn-sm btn-outline-success' onClick={()=>handlesubmit()}>Submit</button>
+        
+        <div className="text-end mt-3">
+          <button
+            className="btn btn-sm btn-outline-success"
+            onClick={() => handlesubmit()}
+          >
+            Submit
+          </button>
         </div>
-     </div>
-     <div className='container mt-5'>
-     <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">No</th>
-      <th scope="col">Firstname</th>
-      <th scope="col">Lastname</th>
-      <th scope="col">Email</th>
-      <th scope='col'>Password</th>
-    </tr>
-  </thead>
-  <tbody>
-    {
-        list.map((student,i)=>{
-            return <tr>
-                <td>{i+1}</td>
-                <td>{student.firstname}</td>
-                <td>{student.lastname}</td>
-                <td>{student.email}</td>
-                <td>{student.password}</td>
+      </div>
+      <div className="container mt-5">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">No</th>
+              <th scope="col">Firstname</th>
+              <th scope="col">Lastname</th>
+              <th scope="col">Email</th>
+              <th scope="col">Password</th>
+              <th scope="col">Location</th>
+              <th scope="col">Hobbies</th>
+             
             </tr>
-        })
-    }
- 
-  </tbody>
-</table>
-     </div>
-     </>
-  )
+          </thead>
+          <tbody>
+            {list.map((student, i) => {
+              return (
+                <tr>
+                  <td>{i + 1}</td>
+                  <td>{student.firstname}</td>
+                  <td>{student.lastname}</td>
+                  <td>{student.email}</td>
+                  <td>{student.password}</td>
+                  <td>{student.location}</td>
+                  <td>{student.hobbies}</td>
+                 
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
 
-export default Form
+export default Form;
